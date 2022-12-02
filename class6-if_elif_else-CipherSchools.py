f4901ac08ac1c1e99aa675bbf99f54e006450720
HereIsAbhi <kabhinav201412@gmail.com>
2022-12-02 12:18:14 +0530

    notes

---
+++ class6-if_elif_else-CipherSchools.py
@@ -0,0 +1,7 @@
+# 1 indent= 2 space,4 space,8 space,tab
+
+****quest: can profile A access profile B
+a:isfreind
+b:isblocked
+c:isadmin
+d:ispichai
---
+++ class7-looping-Cipherschool.py
@@ -0,0 +1,9 @@
+# name="Abhinav"
+# print(type(name))
+# print("***")
+# for c in name:
+#     print(c)
+#     print(type(c))
+
+for i in range(5):
+    print(i)
